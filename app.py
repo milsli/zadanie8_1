331cc70db955544d262720cc9d4244f41de6a5d3
Miłosz <milsli69.gmail.com>
2025-09-19 10:59:45 +0200

    Całakowite przeorganizowanie projektu: folder tworzony tylko w wypadku gdy nie istnieje, powieranie filmu tylko w przypadku gdy filmu brak

--- app.py
+++ app.py
@@ -15,6 +15,16 @@ def compress_mp3(file_path, bitrate="64k"):
 
     os.replace(temp_path, file_path)
 
+@st.cache_resource
+def get_openai_client():
+    return OpenAI(api_key=env["OPENAI_API_KEY"])
+
+def createDirectory(dir_name = ""):
+    dir_exists = os.path.isdir(dir_name)
+    if not dir_exists:
+        os.makedirs(dir_name, exist_ok=True)
+
+
 st.title("Generator streszczeń do wykładów z fizyki")
 env = dotenv_values(".env")
 
@@ -27,116 +37,123 @@ if 'AWS_ACCESS_KEY_ID' in st.secrets:
 if 'AWS_SECRET_ACCESS_KEY' in st.secrets:
     env["AWS_SECRET_ACCESS_KEY"] = st.secrets["AWS_SECRET_ACCESS_KEY"]    
 
-@st.cache_resource
-def get_openai_client():
-    return OpenAI(api_key=env["OPENAI_API_KEY"])
-
-s3 = boto3.client('s3', endpoint_url=env["AWS_ENDPOINT_URL_S3"], aws_access_key_id=env["AWS_ACCESS_KEY_ID"],
+def download(rok):  
+    s3 = boto3.client('s3', endpoint_url=env["AWS_ENDPOINT_URL_S3"], aws_access_key_id=env["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key=env["AWS_SECRET_ACCESS_KEY"])
 
-BUCKET_NAME = "phisicsvideo"
+    BUCKET_NAME = "phisicsvideo"  
 
-def download(rok):    
-    mainDir = "download/" + rok + "/"
-    filmfolder = mainDir + "film"
-    os.makedirs(mainDir, exist_ok=True)
+    main_dir = "download/" + rok + "/"
+    film_dir = main_dir + "film/"
+    audio_dir = main_dir + "audio/"
+    summary_dir = main_dir + "summary/"
+    text_dir = main_dir + "text/"
+
+    createDirectory(main_dir)
+    createDirectory(film_dir)
+    createDirectory(audio_dir)
+    createDirectory(summary_dir)
+    createDirectory(text_dir)
+
     response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=rok)
     filmsNumber = response['KeyCount']
     filmsCounter = 0
-    filmfolder = mainDir + "film"
-    os.mkdir(filmfolder)
     download_progress = st.progress(0)
     download_progress.text("Trwa pobieranie wykładów z roku: " + rok)
+    
+        
     for obj in response["Contents"]: 
         if "mp4" in obj["Key"]:
             film = obj["Key"].replace(rok + '/','')  
             print(film)
-            s3.download_file(BUCKET_NAME, obj["Key"], filmfolder + "/" + film)
+            film_path = film_dir + film
+            if not os.path.exists(film_path):
+                s3.download_file(BUCKET_NAME, obj["Key"],film_path)
+            
         filmsCounter += 1
         download_progress.text("Trwa pobieranie wykładu nr " + str(filmsCounter))
         download_progress.progress(int(100 * filmsCounter / filmsNumber))
     st.text('Zakończono pobieranie za rok ' + rok)
 
-    return mainDir, filmfolder, filmsNumber
+    return film_dir, audio_dir, summary_dir, text_dir, filmsNumber
+
+
+
+
+
 
 def processing(rok):
     st.write("Wybrano rok wykładów : " + rok)
-    mainDir, filmfolder, filmsNumber = download(rok)
+    film_dir, audio_dir, summary_dir, text_dir, filmsNumber = download(rok)
 
-    audiofolder = mainDir + "audio"
-    os.mkdir(audiofolder)
-    textfolder = mainDir + "text"
-    os.mkdir(textfolder)
-    summaryfolder = mainDir + "summary"
-    os.mkdir(summaryfolder)
-
-    processing_progress = st.progress(0)
-    processing_progress.text("Trwa przetwarzanie plików ...")
-    filmsNumber = filmsNumber * 5
-    processedCounter = 0
-    filenames = os.listdir(filmfolder)
-    for name in filenames:
-        processing_progress.text("Wydobywanie audio z pliku " + name)
-        sound = AudioSegment.from_file(filmfolder + "/" + name)
-
-        processedCounter += 1
-        processing_progress.progress(int(100 * processedCounter / filmsNumber))
-
-        start_point = len(sound) / 10
-        stop_point = len(sound) / 2
-        useful_part = sound[start_point:stop_point]
-        mp3FileName = audiofolder + "/" + pathlib.Path(name).stem + ".mp3"
-        useful_part.export(mp3FileName, format="mp3")
-
-        processedCounter += 1
-        processing_progress.progress(int(100 * processedCounter / filmsNumber))
-
-        mp3FileSize = os.path.getsize(mp3FileName)
-        if mp3FileSize > 25 * 1024 * 1024:
-            processing_progress.text("Kompresja pliku mp3 ...")
-            print("Plik mp3 jest zbyt duży, kompresja ...")
-            compress_mp3(file_path = mp3FileName, bitrate="64k")
-        
-        processedCounter += 1
-        processing_progress.progress(int(100 * processedCounter / filmsNumber))
-
-        mp3FileSize = os.path.getsize(mp3FileName)
-        if mp3FileSize > 25 * 1024 * 1024:
-            print("Plik mp3 jest zbyt duży, nawet po kompresji. ")
-            print("W następnej wersji programu ten problem zostanie rozwiązany.")
-            processedCounter += 2
-            continue
-
-        processing_progress.text("Transkrypcja do formatu tekstowego ...")
-        with open(mp3FileName, "rb") as f:
-            transcript = get_openai_client().audio.transcriptions.create(file=f,model="whisper-1")
-
-        textFileName = textfolder + "/" + pathlib.Path(name).stem + ".txt"
-        with open(textFileName, "w") as f:
-            f.write(transcript.text)
-
-        processedCounter += 1
-        processing_progress.progress(int(100 * processedCounter / filmsNumber))
     
-        processing_progress.text("Generowanie i zapisywanie streszczenia ...")
-        summary = get_openai_client().chat.completions.create(
-            model="gpt-4o",
-            messages=[
-                {"role": "system", "content": "Streszczaj teksty w sposób jasny i rzeczowy."},
-                {"role": "user", "content": f"Podsumuj ten tekst:\n\n{transcript.text}"}
-            ],
-            temperature=0.5,      
-        )
+    # processing_progress = st.progress(0)
+    # processing_progress.text("Trwa przetwarzanie plików ...")
+    # filmsNumber = filmsNumber * 5
+    # processedCounter = 0
+    # filenames = os.listdir(film_dir)
+    # for name in filenames:
+    #     processing_progress.text("Wydobywanie audio z pliku " + name)
+    #     sound = AudioSegment.from_file(film_dir + "/" + name)
 
-        # zapisywanie streszczenia
-        summaryFileName = summaryfolder + "/" + pathlib.Path(name).stem + ".txt"
-        with open(summaryFileName, "w") as f:
-                f.write(summary.choices[0].message.content)
+    #     processedCounter += 1
+    #     processing_progress.progress(int(100 * processedCounter / filmsNumber))
 
-        processedCounter += 1
-        processing_progress.progress(int(100 * processedCounter / filmsNumber))
-    processing_progress.text("Zakończono przetwarzanie plików.")
-    processing_progress.progress(100)
+    #     start_point = len(sound) / 10
+    #     stop_point = len(sound) / 2
+    #     useful_part = sound[start_point:stop_point]
+    #     mp3FileName = audio_dir + "/" + pathlib.Path(name).stem + ".mp3"
+    #     useful_part.export(mp3FileName, format="mp3")
+
+    #     processedCounter += 1
+    #     processing_progress.progress(int(100 * processedCounter / filmsNumber))
+
+    #     mp3FileSize = os.path.getsize(mp3FileName)
+    #     if mp3FileSize > 25 * 1024 * 1024:
+    #         processing_progress.text("Kompresja pliku mp3 ...")
+    #         print("Plik mp3 jest zbyt duży, kompresja ...")
+    #         compress_mp3(file_path = mp3FileName, bitrate="64k")
+        
+    #     processedCounter += 1
+    #     processing_progress.progress(int(100 * processedCounter / filmsNumber))
+
+    #     mp3FileSize = os.path.getsize(mp3FileName)
+    #     if mp3FileSize > 25 * 1024 * 1024:
+    #         print("Plik mp3 jest zbyt duży, nawet po kompresji. ")
+    #         print("W następnej wersji programu ten problem zostanie rozwiązany.")
+    #         processedCounter += 2
+    #         continue
+
+    #     processing_progress.text("Transkrypcja do formatu tekstowego ...")
+    #     with open(mp3FileName, "rb") as f:
+    #         transcript = get_openai_client().audio.transcriptions.create(file=f,model="whisper-1")
+
+    #     textFileName = text_dir + "/" + pathlib.Path(name).stem + ".txt"
+    #     with open(textFileName, "w") as f:
+    #         f.write(transcript.text)
+
+    #     processedCounter += 1
+    #     processing_progress.progress(int(100 * processedCounter / filmsNumber))
+    
+    #     processing_progress.text("Generowanie i zapisywanie streszczenia ...")
+    #     summary = get_openai_client().chat.completions.create(
+    #         model="gpt-4o",
+    #         messages=[
+    #             {"role": "system", "content": "Streszczaj teksty w sposób jasny i rzeczowy."},
+    #             {"role": "user", "content": f"Podsumuj ten tekst:\n\n{transcript.text}"}
+    #         ],
+    #         temperature=0.5,      
+    #     )
+
+    #     # zapisywanie streszczenia
+    #     summaryFileName = summary_dir + "/" + pathlib.Path(name).stem + ".txt"
+    #     with open(summaryFileName, "w") as f:
+    #             f.write(summary.choices[0].message.content)
+
+    #     processedCounter += 1
+    #     processing_progress.progress(int(100 * processedCounter / filmsNumber))
+    # processing_progress.text("Zakończono przetwarzanie plików.")
+    # processing_progress.progress(100)
 
 with st.sidebar:
     st.subheader("Podaj rok")
